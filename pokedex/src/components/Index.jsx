8d1478dcbd6fb607c9951a984af4aedbb4d3ccf6
Marco <tassonemarco67@gmail.com>
2024-09-02 20:28:00 +0200

    added search bar and ripositioned circles

--- pokedex/src/components/Index.jsx
+++ pokedex/src/components/Index.jsx
@@ -3,18 +3,26 @@ function Index(){
   
   return (
     <>
-    <div className="container">
+      <div className="container">
 
-      <div className="left-side">
+        <div className="left-side">
+          
+        </div>
+
+        <div className="right-side">
+          <div className="search-bar">
+            <input type="text" placeholder="Search Pokemon" />
+          </div>
+
+          <div className="button-grid">
+
+          </div>
+
+          <div className="circle-top"></div>
+        </div>
+      </div>
       
-     </div>
-
-     <div className="right-side">
-      <div className="circle-top"></div>
-     </div>
-    </div>
-     
-     <div className="circle"></div>
+      <div className="circle"></div>
     </>
   )
 }
