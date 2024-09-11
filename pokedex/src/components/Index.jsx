b6f96db75590ad9bc0605d9d58065fd1d4b4a614
Marco <tassonemarco67@gmail.com>
2024-09-11 19:02:09 +0200

    added white buttons, yellow led and black rectangles

--- pokedex/src/components/Index.jsx
+++ pokedex/src/components/Index.jsx
@@ -27,18 +27,27 @@ function Index(){
                 <div className="button"></div>
                 <div className="button"></div>
               </div>
+              <div className="lines">
+                <div className="line"></div>
+                <div className="line"></div>
+              </div>
+              <div className="button-grid-white">
+                <div className="white-button"></div>
+                <div className="white-button"></div>
+              </div>
+
+              <div className="container-yellow-circle">
+                <div className="yellow-circle"></div>
+              </div>
+              
+              <div className="container-rectangles">
+                <div className="rectangle-left"></div>
+                <div className="rectangle-right"></div>
+              </div>
             </div>
           </div>
-
-          
-
-          
-
-          <div className="circle-top"></div>
         </div>
       </div>
-      
-      <div className="circle"></div>
     </>
   )
 }
